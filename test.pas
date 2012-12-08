e6989126c32cae80d6e5ad4f4e0b0a07e763785c
eucho <appotoxin@gmail.com>
2012-12-08 17:27:42 +0800

    modified sample source

--- test.pas
+++ test.pas
@@ -1,4 +1,17 @@
-
+program test(input,output);
+var 
+    x,y:integer;
+    c:array[1..5] of real;
+    d:record a:integer end;
+    function gtcomdiv(a,b:integer):integer;
+    begin
+      if b=0 then gtcomdiv:=a
+      else gtcomdiv:=gtcomdiv(b,a mod b)
+    end;
+begin
+  read(x,y);
+  write(gtcomdiv(x,y))
+end.
 program test(input);
 var x, y:integer
 begin
